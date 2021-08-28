26096b4e3801dabb30a277175ab9a69b2666b557
cryi <cryi@tutanota.com>
2021-08-29 00:28:56 +0200

    added default port map

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -6,6 +6,18 @@ if _which.exitcode == 0 and _which.stdoutStream ~= nil then
     IP_PATH = _which.stdoutStream:read("a"):gsub("^%s*(.-)%s*$", "%1")
 end
 
+local _portMap = am.app.get_configuration("PORT_MAP")
+if am.app.get_configuration("OUTBOUND_ADDR", false) and not _portMap then 
+    _portMap = {
+        "30666:30666",
+        "30666:30666/udp",
+        "8895:8895",
+        "8895:8895/udp",
+        "8897:8897",
+        "8897:8897/udp"
+    }
+end
+
 am.app.set_model(
     {
         IP_PATH = IP_PATH,
@@ -14,7 +26,8 @@ am.app.set_model(
         SERVICES = {
             ["egem-geth"] = "__egem/assets/daemon.service",
             ["egem-stats"] = "__egem/assets/stats.service"
-        }
+        },
+        PORT_MAP = _portMap
     },
     { merge = true, overwrite = true }
 )
